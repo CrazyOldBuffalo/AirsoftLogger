307aaf90fa0f474eb7e7bb14d77c7b557e473fb9
CrazyOldBuffalo <tomasz.boberek@gmail.com>
2021-04-04 21:52:18 +0100

    Updated Site Details to use LINQ to perform a join instead of just pulling one set of data

--- AirsoftLogger/Controllers/HomeController.cs
+++ AirsoftLogger/Controllers/HomeController.cs
@@ -43,8 +43,38 @@ namespace AirsoftLogger.Controllers
 
         public IActionResult SiteDetails(string id)
         {
+            List<SiteAddress> modelList = new List<SiteAddress>();
+            var query = (from s in _Context.Sites
+                         join a in _Context.Addresses
+                         on s.Postcode equals a.Postcode
+                         where s.SiteCode == id
+                         select new SiteAddress
+                         {
+                             SiteCode = s.SiteCode,
+                             SiteName = s.SiteName,
+                             Website = s.Website,
+                             Tel = s.Tel,
+                             Street = a.Street,
+                             City = a.City,
+                             Postcode = a.Postcode
+                         }).ToList();
+            foreach (var item in query)
+            {
+                modelList.Add(new SiteAddress()
+                {
+                    SiteCode = item.SiteCode,
+                    SiteName = item.SiteName,
+                    Website = item.Website,
+                    Tel = item.Tel,
+                    Street = item.Street,
+                    City = item.City,
+                    Postcode = item.Postcode
+                });
+
+            }
+            Address Test = _Context.Addresses.Find(_Context.Sites.Find(id).Postcode);
             Site SiteDetails = _Context.Sites.Find(id);
-            return View(SiteDetails);
+            return View(query);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
