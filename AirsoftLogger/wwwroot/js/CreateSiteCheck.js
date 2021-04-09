d45fbc05809bd0e14f89975841cc0a7ed1d36388
CrazyOldBuffalo <tomasz.boberek@gmail.com>
2021-04-09 20:31:00 +0100

    Created Event Check code

--- AirsoftLogger/wwwroot/js/CreateSiteCheck.js
+++ AirsoftLogger/wwwroot/js/CreateSiteCheck.js
@@ -6,24 +6,24 @@
     var postcode = $(".PostCode").val();
     var sitename = $(".siteName").val();
 
-    if (sitecode == "") {
-        $("#sitecodewarn").html("A Site MUST have a Sitecode");
+    if (sitecode == "" || sitecode.length < 2) {
+        $("#sitecodewarn").html("A Site MUST have a Valid Sitecode");
         sitecheck = false;
     }
-    if (city == "") {
-        $("#sitecitywarn").html("A Site MUST have a City");
+    if (city == "" || city.length < 2 ) {
+        $("#sitecitywarn").html("A Site MUST have a Valid City");
         sitecheck = false;
     }
-    if (tel == "") {
-        $("#sitetelwarn").html("A Site MUST have a Contact Number");
+    if (tel == "" || tel.length < 4) {
+        $("#sitetelwarn").html("A Site MUST have a Valid Contact Number");
         sitecheck = false;
     }
-    if (postcode == "") {
-        $("#sitepostcodewarn").html("A Site MUST have a Postcode");
+    if (postcode == "" || postcode.length < 4) {
+        $("#sitepostcodewarn").html("A Site MUST have a Valid Postcode");
         sitecheck = false;
     }
-    if (sitename == "") {
-        $("#sitenamewarn").html("A Site MUST hava a Name");
+    if (sitename == "" || sitename.length < 4) {
+        $("#sitenamewarn").html("A Site MUST have a Valid Name");
         sitecheck = false;
     }
 
@@ -31,7 +31,36 @@
         return false;
     }
     else {
+        alert("Site Created");
         return true;
     }
 }
 
+function EventCheck() {
+    var currentdate = new Date().toISOString().slice(0,10);
+    var eventCheck;
+    var sitecode = $(".siteCode").val();
+    var date = $(".Date").val();
+    var spaces = parseInt($(".Spaces").val());
+
+    if (sitecode == "" || sitecode.length <= 2) {
+        $("#eventsitecodewarn").html("Please Enter A Valid Sitecode");
+        eventCheck = false;
+    }
+    if (date == "" || date < currentdate) {
+        $("#eventdatewarn").html("Please Enter A Valid Date");
+        eventCheck = false;
+    }
+    if (spaces == "" || spaces < 1) {
+        $("#eventspaceswarn").html("Please Enter a Valid Number of Spaces");
+        eventCheck = false;
+    }
+
+    if (eventCheck == false) {
+        return false;
+    }
+    else {
+        alert("Event Created");
+        return true;
+    }
+}
