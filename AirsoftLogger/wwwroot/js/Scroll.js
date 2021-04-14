77b9b1443b96d142cb3a0ccdd8491c8d2e82b838
CrazyOldBuffalo <tomasz.boberek@gmail.com>
2021-04-14 16:00:39 +0000

    Quick Final Hotfix for JQuery/JS scripts, Converting Scoll to JQuery

--- AirsoftLogger/wwwroot/js/Scroll.js
+++ AirsoftLogger/wwwroot/js/Scroll.js
@@ -1,13 +1,13 @@
 ﻿$(document).ready(function () {
-    var TopofPage = document.getElementById("Top");
+    var TopofPage = $('#Top');
 
     window.onscroll = function () { scrollFunction() };
 
     function scrollFunction() {
-        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-            TopofPage.style.display = "block";
+        if ($(document.body).scrollTop > 40 || $(document.documentElement).scrollTop > 40) {
+            TopofPage.show();
         } else {
-            TopofPage.style.display = "none";
+            TopofPage.hide();
         }
     }
 
@@ -15,6 +15,6 @@
 });
 
 function toppage() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
+    $(document.body).scrollTop(0);
+    $(document.documentElement).scrollTop(0);
 }
--- AirsoftLogger/wwwroot/js/Slideshow.js
+++ AirsoftLogger/wwwroot/js/Slideshow.js
@@ -17,17 +17,3 @@
 });
 
 
-
-
-//function showslides() {
-//    var i;
-//    var slides = document.getelementsbyclassname("myslides");
-//    for (i = 0; i < slides.length; i++) {
-//        slides[i].style.display = "none";
-//    }
-//    slideindex++;
-//    if (slideindex > slides.length) { slideindex = 1 }
-//    slides[slideindex - 1].style.display = "block";
-//    settimeout(showslides, 2000);
-//}
-
